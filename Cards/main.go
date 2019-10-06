91993fb9d33708181b05119f9105f1b5b1bbcdfe
EricKrg <eric.krueger@uni-jena.de>
2019-10-06 14:03:31 +0200

    some basic stuff

---
+++ Cards/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+var num int // declare outside of func
+var count int = 3
+
+func main() {
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades" // compiler figures out the data type even though go is static typed
+	fmt.Println(card)
+	card = "King" // colon only needed for init
+	fmt.Println(card)
+
+	//var num int
+	num = 10
+	fmt.Println(num)
+	fmt.Println(count)
+	// card from func
+	card2 := newCard()
+	fmt.Println(card2)
+}
+
+// return types
+func newCard() string {
+	return "Queen"
+}
